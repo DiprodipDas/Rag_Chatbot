e9ad6186a6a2c1d3cb91def385e2c7d284001f62
Diprodip Das <diprodip755@gmail.com>
2026-04-11 13:22:59 +0600

    Text structure-based spliting sucessfully done

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,10 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 loader = PyPDFLoader("C:\RAG_Chatbot\Dipro2.pdf")
 docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
+split_docs = text_splitter.split_documents(docs)
+
+print(split_docs[0])
