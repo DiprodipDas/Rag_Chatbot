fa6121e83c316c208b6df2c555c929e6495971a3
Diprodip Das <diprodip755@gmail.com>
2026-04-11 14:33:03 +0600

    embeddings objects created successfully

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
 
 loader = PyPDFLoader("C:\RAG_Chatbot\Dipro2.pdf")
 docs = loader.load()
@@ -7,4 +8,5 @@ docs = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
 split_docs = text_splitter.split_documents(docs)
 
-print(split_docs[0])
+embeddings= OllamaEmbeddings(model="gemma:2b")
+
